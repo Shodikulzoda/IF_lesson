f0e54575c5c431b969f70a21f0293b8cec4c0e42
Alisher <shodikulzoda@gmail.com>
2025-01-04 14:02:19 +0500

    if and switch constrution and some tasks

--- lesson_IF/Program.cs
+++ lesson_IF/Program.cs
@@ -20,37 +20,49 @@
 //Console.WriteLine($"a={a} b={b}");
 
 //zadacha 2
-Console.WriteLine("Введите первое число");
-int operand1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int operand2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите операцию");
-string? symbol = Console.ReadLine();
-double result = 0;
-switch (symbol)
-{
-    case "+":
-        result = operand1 + operand2;
+//Console.WriteLine("Введите первое число");
+//int operand1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите второе число");
+//int operand2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите операцию");
+//string? symbol = Console.ReadLine();
+//double result = 0;
+//switch (symbol)
+//{
+//    case "+":
+//        result = operand1 + operand2;
         
-        break;
-    case "-":
-        result = operand1 - operand2;
-        break;
-    case "*":
-        result = operand1 * operand2;   
-        break;
-    case "/":
-        if (operand2 == 0)
-        {
-            Console.WriteLine("На ноль делить нельзя");
-        }
-        else
-        {
-            result = operand1 / operand2;
-        }
-        break;
-    default:
-        Console.WriteLine("Неизвестная операция");
+//        break;
+//    case "-":
+//        result = operand1 - operand2;
+//        break;
+//    case "*":
+//        result = operand1 * operand2;   
+//        break;
+//    case "/":
+//        if (operand2 == 0)
+//        {
+//            Console.WriteLine("На ноль делить нельзя");
+//        }
+//        else
+//        {
+//            result = operand1 / operand2;
+//        }
+//        break;
+//    default:
+//        Console.WriteLine("Неизвестная операция");
+//        break;
+//}
+//Console.WriteLine($" результат= { result}");
+
+
+//zadacha 3
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+    case int n when (number >= 0 && number <= 14):
+
+        Console.WriteLine("Число в диапазоне от 0 до 14");
         break;
 }
-Console.WriteLine($" результат= { result}");
